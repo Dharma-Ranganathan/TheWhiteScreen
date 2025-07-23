342c0d83c618a8dc20513d4a00e1f6e18500c33d
Dharma Ranganathan <dharmaqubla369@gmail.com>
2025-07-23 17:05:42 +0530

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB3_OXr7Fwy7RjEOdY8R7SIenp7VysfA5k",
-  authDomain: "thewhitescreen-2ac59.firebaseapp.com",
-  projectId: "thewhitescreen-2ac59",
-  storageBucket: "thewhitescreen-2ac59.firebasestorage.app",
-  messagingSenderId: "505544978905",
-  appId: "1:505544978905:web:1e9f85617af528400529c8",
+  apiKey: "<secret>",
+  authDomain: "<secret>",
+  projectId: "<secret>",
+  storageBucket: "<secret>",
+  messagingSenderId: "<secret>",
+  appId: "<secret>",
 };
 
 // Initialize Firebase
